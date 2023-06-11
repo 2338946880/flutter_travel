7e97279cc55f2702565b6fa24c10aa05f987d4dc
Tui <2338946880@qq.com>
2023-06-11 19:10:00 +0800

    Initial commit

---
+++ lib/video_clip/video_clipper.dart
@@ -0,0 +1,9 @@
+library video_clipper;
+
+export 'package:flutter_travel/video_clip/video_viewer.dart';
+export 'package:flutter_travel/video_clip/clipper.dart';
+export 'package:flutter_travel/video_clip/clip_editor.dart';
+export 'package:flutter_travel/video_clip/clip_editor_painter.dart';
+export 'package:flutter_travel/video_clip/thumbnail_viewer.dart';
+export 'package:flutter_travel/video_clip/file_formats.dart';
+export 'package:flutter_travel/video_clip/storage_dir.dart';
